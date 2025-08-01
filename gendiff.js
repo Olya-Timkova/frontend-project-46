90767b0d31855df73aba82585f4f113192ea7620
Olya-Timkova <o.timkova@bk.ru>
2025-08-01 16:55:44 +0300

    step2

--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
 const { Command } = require('commander');
-const packageJson = require('./package.json'); // предполагается, что версия берётся из package.json
+const packageJson = require('./package.json');
 
 const program = new Command();
 
@@ -7,6 +8,13 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version(packageJson.version, '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command');
-
-program.parse();
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing files:
+      File 1: ${filepath1}
+      File 2: ${filepath2}
+      Format: ${options.format}`);
+  })
+  .parse(process.argv);
